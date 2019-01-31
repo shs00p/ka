3ff0108f8320ce51e1765be3249dcd4be737ac8b
shs00p <shs00p@163.com>
2019-01-31 16:03:41 +0800

    cs

--- cs.php
+++ cs.php
@@ -5,14 +5,14 @@ header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 $name=$_POST['username'];
 $pwd=$_POST['password'];
-$con = mysqli_connect('bdm25683025.my3w.com','bdm25683025','3315315xx','bdm25683025_db');//链接数据库
+$con = mysqli_connect('bdm25683025.my3w.com','bdm25683025','3315315Xx','bdm25683025_db');//链接数据库
 //mysql_set_charset($link,'utf8'); //设定字符集
  
 if(!$con){ 
     die('error:'.mysql_error()); 
 }
  
-$sql_select="select name,password from user where username= ?"; //从数据库查询信息
+$sql_select="select name,password from user where name= ?"; //从数据库查询信息
 $stmt=mysqli_prepare($con,$sql_select);
  mysqli_stmt_bind_param($stmt,'s',$name);
  mysqli_stmt_execute($stmt);
