729ed5f33a4bf07c3792f4aded09d149d8ac61f3
shs00p <shs00p@163.com>
2019-05-01 10:08:39 +0800

    5.1 home

--- ajax/getPn.php
+++ ajax/getPn.php
@@ -5,17 +5,18 @@ header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 include '../conn.php';
 
-if(isset($_POST['state']) && !empty($_POST['state'])) {
-  $sql = "select id,name,from_unixtime(sime,'%m-%d %H:%i') sime,sime usime from punch where tid = 2 and state = 0 order by id asc";
-  $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      $sjrr = 1;
-      echo $sjrr;
-      $conn->close();
-      exit;
-    }
-}
-else {
+// $state = $_POST['state'];
+// echo $state;
+// exit;
+
+$sql = "select id from punch where tid = 2 and state = 0";
+$result = $conn->query($sql);
+  if ($result->num_rows > 0 && empty($_POST['state'])) {
+    echo $_POST['state'];
+    $conn->close();
+    exit;
+  }
+  else {
   $srr = array();
   $sql = "select a.id,a.name,a.aid,IFNULL(b.bid,0) bid from (SELECT a.id,a.name,IFNULL(b.cid,0) aid from event a LEFT JOIN (SELECT pid,name,COUNT(id) cid from event where pid <> 0 GROUP BY pid) b on a.id = b.pid where a.pid = 0) a left JOIN (SELECT COUNT(id) bid,name,pid from punch GROUP BY pid) b on a.id = b.pid order by id";
     $result = $conn->query($sql);
@@ -31,7 +32,8 @@ else {
       $sjrr = json_encode($srr);
       echo $sjrr;
       $conn->close();
-}
+  }
+
 //$state = null;
 
 //echo $state;
