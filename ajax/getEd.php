1035b4b8dd451197f9d2a4fb51d2273a411a7930
shs00p <shs00p@163.com>
2019-05-24 17:47:15 +0800

    5.24 edit

--- ajax/getEd.php
+++ ajax/getEd.php
@@ -4,7 +4,7 @@ include '../conn.php';
 $id = $_POST['id'];
 //$type = 1;
 
-  $sql = "select id,name,from_unixtime(sime,'%Y-%m-%d %H:%i') sime,from_unixtime(eime,'%Y-%m-%d %H:%i') eime,tid from punch where id = '$id'";
+  $sql = "select id,name,from_unixtime(sime,'%Y-%m-%d %H:%i') sime,from_unixtime(eime,'%Y-%m-%d %H:%i') eime,tid,remarks from punch where id = '$id'";
   $result = $conn->query($sql);
     if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
